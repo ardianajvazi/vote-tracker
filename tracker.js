f9702f7b69cf5d4bfec913be789a2a7f8b920e85
ardianajvazi <ardian_46@hotmail.com>
2015-11-09 15:57:10 -0800

    First commit

---
+++ tracker.js
@@ -0,0 +1,25 @@
+var Photo = function(path) {
+this.path = path;
+this.photos = [];
+
+
+
+}
+
+
+var aston martin = new Photo ('img/aston-martin.jpg');
+var audi = new Photo ('img/audi.png');
+var bentley = new Photo ('img/Bentley.png');
+var benz = new Photo ('img/Benz.png');
+var bmw = new Photo ('img/BMW.png');
+var bugatti = new Photo ('img/bugatti.jpg');
+var ferrari = new Photo ('img/ferrari.jpg');
+var lamborghini = new Photo ('img/lamborghini.png');
+var maserati = new Photo ('img/Maserati.png');
+var porsche = new Photo ('img/porsche.png');
+var rolls royce = new Photo ('img/rolls-royce.jpg');
+var tesla = new Photo ('img/tesla.png');
+
+
+
+]
